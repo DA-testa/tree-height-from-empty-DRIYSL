94faa8072cdc8993c2d8c7e1a0419f85ad8d2e60
DANIELS RAIVO IVANOVS 211RMB021 <118028746+DRIYSL@users.noreply.github.com>
2023-03-05 21:33:35 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,27 +1,55 @@
 # python3
+#211RMB021 Daniels Raivo Ivanovs 15.grupa
 
 import sys
 import threading
 import numpy
 
 
-def compute_height(n, parents):
-    # Write this function
+def compute_height(n, parents:):
+    heights = np.zeros(int(n))
     max_height = 0
-    # Your code here
+    for i in range(int(n)):
+        if heights[i] > 0:
+            continue
+         height = 0
+        j =1 
+        while j! = -1:
+            if heights [j] >0:
+                height += heights[j]
+                break
+               else:
+                height +=1
+                j= int(parents[j])
+              heights[i] = height
+              if heights > max_height:
+                    max_height = height
     return max_height
 
-
 def main():
-    # implement input form keyboard and from files
+    input_method = input().strip()
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
+    if input_method == "F":
+        try:
+            file_name = input().strip()
+            with open(f"./test/{file_name}") as file:
+                n = file.readline().strip()
+                parents = file.readline().strip().split()
+       except:
+                print("ERROR")
+                return
+         elif input_method == "I":
+            n = input().strip()
+            parents = input().strip().split()
+          else:
+            print("Invalid input method.")
+            return
+        
+        height = compute_height(n,parents)
+        print(int(height))
+        
+                    
     
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
